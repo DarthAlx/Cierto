a8611ada4b380e3cdf8531b9bdde4d480f58e307
Alexis Morales <alx.morales@hadoukendev.com>
2017-08-16 21:12:38 -0500

    2.0

---
+++ index2.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html class="">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Cierto Global</title>
+    <meta name="description" content="Cierto Global Org">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link rel="stylesheet" href="styles/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/font-awesome.css">
+
+
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <div class="page">
+      <div class="barra">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="columna col-sm-1 col-xs-3">
+              <div class="menu">
+                <i class="fa fa-bars fa-2x" aria-hidden="true"></i>
+              </div>
+            </div>
+            <div class="col-sm-11 col-xs-9">
+              <div class="logo">
+                <img class="img-responsive" src="img/logo.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <section class="entrada">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-xs-12 fondo">
+<p>&nbsp;</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+    </div>
+  </body>
+</html>
