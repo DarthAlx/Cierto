0d74944790e826a46a05dc93397f4f6de4536661
Alexis Morales <alx.morales@hadoukendev.com>
2017-08-17 22:07:15 -0500

    servicios

--- index2.php
+++ index2.php
@@ -9,6 +9,7 @@
 
     <link rel="stylesheet" href="styles/bootstrap.min.css">
     <link rel="stylesheet" href="styles/font-awesome.css">
+    <link href="https://fonts.googleapis.com/css?family=Amaranth:400,400i,700,700i|Lato:100,100i,300,300i,400,400i,700,700i,900,900i|Merriweather:300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
 
 
     <link rel="stylesheet" href="css/main.css">
@@ -32,11 +33,177 @@
         </div>
       </div>
 
-      <section class="entrada">
+      <section class="entrada inicio">
         <div class="container-fluid">
           <div class="row">
             <div class="col-xs-12 fondo">
-<p>&nbsp;</p>
+              <div class="row">
+                <div class="col-md-6 cuadroblanco col-md-offset-6">
+                  <span class="linea"></span>
+                  <h3>CIERTO</h3>
+                  <h4>Centro de Investigación, Entrenamiento y Reclutamiento del Trabajador Organizado</h4>
+                  <p>CIERTO es un Contratista de Trabajo Agrícola certificado que recluta, entrena y coloca a trabajadores agrícolas experimentados de México en granjas de los Estados Unidos. Nuestra misión es crear una fuerza de trabajo agrícola profesional cualificada y certificada que aporte valor agregado a la cadena de suministro de alimentos.</p>
+                  <a href="#" class="btn btn-primary btn-lg pull-right">Saber más</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="servicios inicio">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-xs-12 fondo">
+              <div class="row">
+                <div class="col-md-12">
+
+                  <h3>NUESTRO SERVICIO</h3>
+                  <p>Nuestro proceso probado de seis pasos está diseñado para satisfacer las necesidades y representar los mejores intereses tanto de los productores como de los trabajadores agrícolas.</p>
+                  <p>&nbsp;</p>
+                  <div class="container-fluid">
+                    <div class="row">
+                      <div class="col-md-4 servicio">
+                        <div class="overlay">
+                          <div class="text col-xs-12">
+                            <p>Priorizamos las relaciones sobre las transacciones.</p>
+                          </div>
+                        </div>
+                        <div class="numero col-xs-4">
+                          01
+                        </div>
+                        <div class="texto col-xs-8">
+                          Consulta inicial <br> y selección
+                        </div>
+                      </div>
+                      <div class="col-md-4 servicio">
+                        <div class="overlay">
+                          <div class="text col-xs-12">
+                            <p>La transparencia y la integridad en el proceso de contratación son fundamentales para nuestro trabajo.</p>
+                          </div>
+                        </div>
+                        <div class="numero col-xs-4">
+                          02
+                        </div>
+                        <div class="texto col-xs-8">
+                          Reclutamiento de mano de obra de confianza
+                        </div>
+                      </div>
+                      <div class="col-md-4 servicio">
+                        <div class="overlay">
+                          <div class="text col-xs-12">
+                            <p>Todo está en los detalles.</p>
+                          </div>
+                        </div>
+                        <div class="numero col-xs-4">
+                          03
+                        </div>
+                        <div class="texto col-xs-8">
+                          Petición de visa H-2A
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="row">
+                      <div class="col-md-4 servicio">
+                        <div class="overlay">
+                          <div class="text col-xs-12">
+                            <p>Profesionalizamos el trabajo agrícola.</p>
+                          </div>
+                        </div>
+                        <div class="numero col-xs-4">
+                          04
+                        </div>
+                        <div class="texto col-xs-8">
+                          Apoyo y evaluación
+                        </div>
+                      </div>
+                      <div class="col-md-4 servicio">
+                        <div class="overlay">
+                          <div class="text col-xs-12">
+                            <p>Entregamos.</p>
+                          </div>
+                        </div>
+                        <div class="numero col-xs-4">
+                          05
+                        </div>
+                        <div class="texto col-xs-8">
+                          Viajes y orientación
+                        </div>
+                      </div>
+                      <div class="col-md-4 servicio">
+                        <div class="overlay">
+                          <div class="text col-xs-12">
+                            <p>Estamos contigo hasta el final.</p>
+                          </div>
+                        </div>
+                        <div class="numero col-xs-4">
+                          06
+                        </div>
+                        <div class="texto col-xs-8">
+                          Entrenamiento
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <p>&nbsp;</p>
+                  <a href="#" class="btn btn-primary btn-lg pull-right">Saber más</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="seccion diferentes">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-xs-12 fondo">
+              <div class="row">
+                <div class="col-md-6 cuadroblanco col-md-offset-6">
+                  <span class="linea"></span>
+                  <h3>LO QUE NOS HACE DIFERENTES</h3>
+                  <p>&nbsp;</p>
+                  <p>Nuestros procesos de contratación, reclutamiento, capacitación y administración son confiables y transparentes, eliminamos prácticas fraudulentas y protegemos los intereses de productores y trabajadores, nuestros procesos de contratación están supervisados para garantizar integridad.</p>
+                  <a href="#" class="btn btn-primary btn-lg pull-right">Saber más</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="seccion entrenamiento">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-xs-12 fondo">
+              <div class="row">
+                <div class="col-md-6 cuadroblanco col-md-offset-6">
+                  <span class="linea"></span>
+                  <h3>ENTRENAMIENTO</h3>
+                  <p>&nbsp;</p>
+                  <p>El Centro de Investigación, Entrenamiento y Reclutamiento del Trabajador Organizado (CIERTO),  implementa una metodología que busca profesionalizar a los trabajadores/as del campo para mejorar la cadena productiva, reconociendo el valor y la dignidad de cada uno.</p>
+                  <a href="#" class="btn btn-primary btn-lg pull-right">Saber más</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="seccion contacto">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-xs-12 fondo">
+              <div class="row">
+                <div class="col-md-6 cuadroblanco col-md-offset-6">
+                  <span class="linea"></span>
+                  <h3>CONTÁCTANOS</h3>
+                  <p>&nbsp;</p>
+                  <p>Si tienes alguna pregunta envíanos un mensaje y nosotros nos pondrémos en contacto contigo.</p>
+                  <a href="#" class="btn btn-primary btn-lg pull-right">Saber más</a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
